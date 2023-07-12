74af66077bfd135c6d3e3c54c53a11903b213604
Wicked98 <136556282+Wicked98@users.noreply.github.com>
2023-07-12 13:13:16 +1000

    Added crewmates list with names & hobbies

--- Assets/Scripts/Crewmate.cs
+++ Assets/Scripts/Crewmate.cs
@@ -10,12 +10,47 @@ public class Crewmate
     public string crewmateName;
     public bool hasParasite;
     public string crewmateHobby;
-
-    public Crewmate(string crewmateName, bool hasParasite, string crewmateHobby)
+    private List<string> possibleNames = new()
     {
-        this.crewmateName = crewmateName;
-        this.hasParasite = hasParasite;
-        this.crewmateHobby = crewmateHobby;
+        "Alfred",
+        "Tim",
+        "Tam",
+        "Maurice"
+    };
+    private List<string> possibleHumanHobbies = new()
+    {
+        "Likes to go fishing",
+        "Tim",
+        "Tam",
+        "Maurice"
+    };
+    private List<string> possibleImposterHobbies = new()
+    {
+        "Alfred",
+        "Tim",
+        "Tam",
+        "Maurice"
+    };
+    public Crewmate()
+    {
+        crewmateName = possibleNames[Random.Range(0, possibleNames.Count)];
+        if (Random.Range(0, 2)== 0)
+        {
+            hasParasite = true; 
+            crewmateHobby = possibleImposterHobbies[Random.Range(0, possibleImposterHobbies.Count)];
+        }
+            else
+        {
+            hasParasite = false; 
+            crewmateHobby = possibleHumanHobbies[Random.Range(0, possibleHumanHobbies.Count)]; 
+        }
     }
+
+    //public Crewmate(string crewmateName, bool hasParasite, string crewmateHobby)
+    //{
+    //    this.crewmateName = crewmateName;
+    //    this.hasParasite = hasParasite;
+    //    this.crewmateHobby = crewmateHobby;
+    //}
 
 }
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ship : MonoBehaviour
 {
-    
-   [SerializeField]public List<Crewmate> crewmates = new(); 
-    [SerializeField]public Crewmate applyingCrewmate;
+    public TextMeshPro nameText;
+    public TextMeshPro hobbyText;
+    [SerializeField] public List<Crewmate> crewmates = new();
+    //[SerializeField] public List<Crewmate> applyingCrewmates = new(); 
+    [SerializeField] public Crewmate applyingCrewmate;
 
     private void Start()
     {
-        applyingCrewmate = new Crewmate("Alfred", false, "Likes to go fishing");
+        CreateCremate();
+
     }
 
     public void OnHireButtonPress()
@@ -18,10 +22,23 @@ public class Ship : MonoBehaviour
         Debug.Log("Hire");
         crewmates.Add(applyingCrewmate);
         applyingCrewmate = null;
+        CreateCremate();
+
     }
 
     public void OnRejectButtonPress()
     {
         Debug.Log("Reject");
+        applyingCrewmate = null;
+        CreateCremate();
+
+    }
+
+    public void CreateCremate()
+    {
+        applyingCrewmate = new Crewmate();
+        nameText.text = applyingCrewmate.crewmateName;
+        hobbyText.text = applyingCrewmate.crewmateHobby;
     }
 }
+
